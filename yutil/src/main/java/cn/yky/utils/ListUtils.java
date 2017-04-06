425d00cc9ef66320871105696d469b3270782aaf
于阔源 <yukuoyuan@hotmail.com>
2017-04-06 11:52:29 +0800

    添加了类库的引用

---
+++ yutil/src/main/java/cn/yky/utils/ListUtils.java
@@ -0,0 +1,19 @@
+package cn.yky.utils;
+
+import java.util.List;
+
+/**
+ * Created by yukoyuan on 16/7/8.
+ * 这是一个集合的工具类
+ */
+public class ListUtils {
+    /**
+     * 这是一个判断集合是否为空的方法
+     *
+     * @param sourceList 穿进去一个集合
+     * @return 是否是空的
+     */
+    public static <V> boolean isEmpty(List<V> sourceList) {
+        return (sourceList == null || sourceList.size() == 0);
+    }
+}
